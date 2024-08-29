d69266dbd93b216a2d693b241d3bd50ebb50ed20
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-08-29 13:33:05 +0300

    vectors 2

---
+++ seventh.R
@@ -0,0 +1,17 @@
+# # country <- c("italy","canada","egyp")
+# # country <- c("italy","canada","egyp")
+# # > codes <- c(italy=380,canada=124,"egyp")
+# # Warning message:
+# #   In formals(fun) : argument is not a function
+# # > codes <- c(italy=380,canada=124,egyp=818)
+# class(codes)
+# [1] "numeric"
+# > 
+#   > class(country)
+# [1] "character"
+# 
+# codes
+# italy canada   egyp 
+# 380    124    818 
+#  country
+# [1] "italy"  "canada" "egyp"  
